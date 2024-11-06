e15c1162713d683dcc4da4d0490a2b02c3d09e0a
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-06 14:34:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ location = st.sidebar.selectbox("Choose a Location:", ["Bali", "India"])
 st.image("https://raw.githubusercontent.com/antoniusawe/sales-report/main/images/house_of_om-removebg-preview.png",  
          use_column_width=True)
 st.markdown("<h1 style='text-align: center; font-size: 50px;'>HOUSE OF OM - DASHBOARD</h1>", unsafe_allow_html=True)
+
+if location == "Bali":
+    # Sub-dropdown for specific options under "Bali"
+    bali_option = st.sidebar.selectbox("Choose a Section:", ["Overview", "Location", "Batch"])
+
+elif location == "India":
+    # Dropdown for program selection when location is "India"
+    program = st.selectbox("Choose a Program:", ["200HR", "300HR"])
