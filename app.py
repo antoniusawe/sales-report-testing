7e76ce7b23bbc9dc8dbb3a75ecb4263e62d42560
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-06 14:33:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ from datetime import timedelta
 
 # Sidebar dropdown for location
 location = st.sidebar.selectbox("Choose a Location:", ["Bali", "India"])
+
+# Display the main image and title
+st.image("https://raw.githubusercontent.com/antoniusawe/sales-report/main/images/house_of_om-removebg-preview.png",  
+         use_column_width=True)
+st.markdown("<h1 style='text-align: center; font-size: 50px;'>HOUSE OF OM - DASHBOARD</h1>", unsafe_allow_html=True)
