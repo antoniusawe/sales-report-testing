6daa5c8753de7ef2d87d325062709f291bab86e2
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-06 14:32:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+from streamlit_echarts import st_echarts
+from datetime import datetime
+from datetime import timedelta
 
+# Sidebar dropdown for location
+location = st.sidebar.selectbox("Choose a Location:", ["Bali", "India"])
