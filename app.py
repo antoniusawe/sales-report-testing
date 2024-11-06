a369868e38904a938880f3760f13eedd36985f76
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-06 15:10:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_echarts import st_echarts
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Sidebar dropdown for location
 location = st.sidebar.selectbox("Choose a Location:", ["Bali", "India"])
@@ -38,18 +38,15 @@ if location == "Bali":
         st.error("Failed to load data. Please check the URL or your connection.")
         st.write(f"Error: {e}")
 
-    # Program dropdown for "Overview" and "Batch" sections
-    if bali_option in ["Overview", "Batch"]:
-        program = st.selectbox("Choose a Program:", ["200HR", "300HR"], key=f"program_{bali_option.lower()}")
+    # Dropdown for program selection when location is "Bali"
+    program = st.selectbox("Choose a Program:", ["200HR", "300HR"], key="program_bali")
 
-    # Overview section
     if bali_option == "Overview":
-        # Year selection for "Overview"
+        # Year selection
         if 'Year' in bali_sales_data.columns:
             unique_years = bali_sales_data['Year'].dropna().unique()
             unique_years = sorted(unique_years)
             unique_years = ["All"] + list(unique_years)
-            
             selected_year = st.radio("Select a Year:", unique_years, key="year_selection")
         else:
             st.warning("Year data not found in the dataset.")
@@ -65,45 +62,87 @@ if location == "Bali":
         else:
             selected_month = "All"
 
-        # Filter data based on program, year, and month for "Overview"
-        if program == "200HR":
-            data_200hr_bali = bali_sales_data[(bali_sales_data['Category'] == '200HR')]
-            if selected_year != "All":
-                data_200hr_bali = data_200hr_bali[data_200hr_bali['Year'] == selected_year]
-            if selected_month != "All":
-                month_num = datetime.strptime(selected_month, '%B').month
-                data_200hr_bali = data_200hr_bali[data_200hr_bali['Batch start date'].dt.month == month_num]
-            st.write("Overview for 200HR Program")
-            st.write(data_200hr_bali)
+        # Filter data based on program, year, and month
+        filtered_data = bali_sales_data[bali_sales_data['Category'] == program]
+        if selected_year != "All":
+            filtered_data = filtered_data[filtered_data['Year'] == selected_year]
+        if selected_month != "All":
+            month_num = datetime.strptime(selected_month, '%B').month
+            filtered_data = filtered_data[filtered_data['Batch start date'].dt.month == month_num]
 
-        elif program == "300HR":
-            data_300hr_bali = bali_sales_data[(bali_sales_data['Category'] == '300HR')]
-            if selected_year != "All":
-                data_300hr_bali = data_300hr_bali[data_300hr_bali['Year'] == selected_year]
-            if selected_month != "All":
-                month_num = datetime.strptime(selected_month, '%B').month
-                data_300hr_bali = data_300hr_bali[data_300hr_bali['Batch start date'].dt.month == month_num]
-            st.write("Overview for 300HR Program")
-            st.write(data_300hr_bali)
+        # Calculate metrics
+        newest_batch_date = filtered_data['Batch start date'].max()
+        cut_off_date = newest_batch_date.strftime('%d %b %Y') if pd.notnull(newest_batch_date) else "No date available"
+        total_booking_ctr = filtered_data[filtered_data["BALANCE"] == 0]["NAME"].count()
+        total_paid_amount = filtered_data[filtered_data["BALANCE"] == 0]["PAID"].sum()
+        average_occupancy = bali_occupancy_data['Occupancy'].mean()
 
-    # Location section
-    elif bali_option == "Location":
-        st.write("Location details for Bali")
-        # You can add specific location-based information or analysis here, like maps, venue details, etc.
+        # Display metrics
+        st.markdown(f"""
+        <div style='text-align: left;'>
+            <div style='font-size: 16px; color: #333333;'>Cut-off data: {cut_off_date}</div>
+        </div>
+        <div style='display: flex; justify-content: center; gap: 50px; padding: 20px;'>
+            <div style='text-align: left;'>
+                <div style='font-size: 16px; color: #333333;'>Total Booking</div>
+                <div style='font-size: 48px;'>{total_booking_ctr}</div>
+                <div style='color: #202fb2; font-size: 18px;'>Number of students</div>
+            </div>
+            <div style='text-align: left;'>
+                <div style='font-size: 16px; color: #333333;'>Amount</div>
+                <div style='font-size: 48px;'>{total_paid_amount:,.0f}</div>
+                <div style='color: #202fb2; font-size: 18px;'>in USD or USD equiv</div>
+            </div>
+            <div style='text-align: left;'>
+                <div style='font-size: 16px; color: #333333;'>Occupancy</div>
+                <div style='font-size: 48px;'>{average_occupancy:.2f}%</div>
+                <div style='color: #202fb2; font-size: 18px;'>Occupancy Rate</div>
+            </div>
+        </div>
+        """, unsafe_allow_html=True)
 
-    # Batch section
-    elif bali_option == "Batch":
-        # Batch details based on program selection
-        st.write(f"Batch details for {program} program")
-        if program == "200HR":
-            data_200hr_batches = bali_sales_data[bali_sales_data['Category'] == '200HR']
-            st.write("Batch Data for 200HR Program")
-            st.write(data_200hr_batches)
-        elif program == "300HR":
-            data_300hr_batches = bali_sales_data[bali_sales_data['Category'] == '300HR']
-            st.write("Batch Data for 300HR Program")
-            st.write(data_300hr_batches)
+        # Bar chart data preparation
+        site_fill_data = bali_occupancy_data.groupby('Site')['Fill'].sum().reset_index().sort_values(by='Fill', ascending=False)
+        room_fill_data = bali_occupancy_data.groupby('Room')['Fill'].sum().reset_index().sort_values(by='Fill', ascending=False)
+        month_counts = filtered_data[filtered_data['BALANCE'] == 0].groupby('Month')['NAME'].count().reset_index()
+
+        # Bar chart configurations
+        highest_fill_value_site = site_fill_data['Fill'].max()
+        highest_fill_value_room = room_fill_data['Fill'].max()
+        highest_value_month = month_counts['NAME'].max()
+
+        # Configure and display bar charts
+        site_bar_chart_data = {
+            "title": {"text": "Top Frequent Sites", "left": "center"},
+            "tooltip": {"trigger": "item", "formatter": "{b}: {c}"},
+            "xAxis": {"type": "category", "data": site_fill_data['Site'].tolist()},
+            "yAxis": {"type": "value"},
+            "series": [{"data": [{"value": fill, "itemStyle": {"color": "#FF5733" if fill == highest_fill_value_site else "#5470C6"}} for fill in site_fill_data['Fill']], "type": "bar"}]
+        }
+
+        room_bar_chart_data = {
+            "title": {"text": "Top Frequent Rooms", "left": "center"},
+            "tooltip": {"trigger": "item", "formatter": "{b}: {c}"},
+            "xAxis": {"type": "category", "data": room_fill_data['Room'].tolist()},
+            "yAxis": {"type": "value"},
+            "series": [{"data": [{"value": fill, "itemStyle": {"color": "#FF5733" if fill == highest_fill_value_room else "#5470C6"}} for fill in room_fill_data['Fill']], "type": "bar"}]
+        }
+
+        month_bar_chart_data = {
+            "title": {"text": "Top Months", "left": "center"},
+            "tooltip": {"trigger": "item", "formatter": "{b}: {c}"},
+            "xAxis": {"type": "category", "data": month_counts['Month'].tolist()},
+            "yAxis": {"type": "value"},
+            "series": [{"data": [{"value": count, "itemStyle": {"color": "#FF5733" if count == highest_value_month else "#5470C6"}} for count in month_counts['NAME']], "type": "bar"}]
+        }
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st_echarts(options=site_bar_chart_data, height="300px")
+        with col2:
+            st_echarts(options=room_bar_chart_data, height="300px")
+        with col3:
+            st_echarts(options=month_bar_chart_data, height="300px")
 
 elif location == "India":
-    # Dropdown for program selection when location is "India"
     program = st.selectbox("Choose a Program:", ["200HR", "300HR"], key="program_india")
